f1ab43f77ab41a314d90971d646f332beb929c63
Hannah Jackson <167377119+WheresHannah@users.noreply.github.com>
2024-06-07 23:15:09 +0000

    navbar and jumbotron

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">A Warm Welcome</h1>
+      <p className="lead">Ye to misery wisdom plenty polite to as. Prepared interest proposal it he exercise. My wishing an in attempt ferrars. Visited eat you why service looking engaged. At place no walls hopes rooms fully in. Roof hope shy tore leaf joy paid boy. Noisier out brought entered detract because sitting sir. Fat put occasion rendered off humanity has.</p>
+      <hr className="my-4" />
+      <a className="btn btn-primary btn-lg" href="#" role="button">Call To Action!</a>
+    </div>
+  );
+}
+
+export default Jumbotron;

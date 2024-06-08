ea98e435df51c140997e7264bcea55e4b552d9cf
Hannah Jackson <167377119+WheresHannah@users.noreply.github.com>
2024-06-08 00:35:44 +0000

    Navbar and jumbootron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 function Jumbotron() {
   return (
-    <div className="jumbotron">
-      <h1 className="display-4">A Warm Welcome</h1>
-      <p className="lead">Ye to misery wisdom plenty polite to as. Prepared interest proposal it he exercise. My wishing an in attempt ferrars. Visited eat you why service looking engaged. At place no walls hopes rooms fully in. Roof hope shy tore leaf joy paid boy. Noisier out brought entered detract because sitting sir. Fat put occasion rendered off humanity has.</p>
-      <hr className="my-4" />
-      <a className="btn btn-primary btn-lg" href="#" role="button">Call To Action!</a>
+    <div className="container">
+      <div className="jumbotron bg-light mt-4 mb-4 p-4 pb-5">
+        <h1 className="display-4 mb-4">A Warm Welcome!</h1>
+        <p className="lead mb-4">Ye to misery wisdom plenty polite to as. Prepared interest proposal it he exercise. My wishing an in attempt ferrars. Visited eat you why service looking engaged. At place no walls hopes rooms fully in. Roof hope shy tore leaf joy paid boy. Noisier out brought entered detract because sitting sir. Fat put occasion rendered off humanity has.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Call To Action!</a>
+      </div>
     </div>
   );
 }
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Start Bootstrap</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Services</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Contact</a>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
